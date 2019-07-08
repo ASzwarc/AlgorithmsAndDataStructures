b78cc6e7efa3d51bc6545439a5954e54bf89a7dd
ASzwarc <szwarc.adam@gmail.com>
2019-07-09 00:15:00 +0200

    Implementation of adding nodes and printing nodes and linked list

--- LinkedList.py
+++ LinkedList.py
@@ -9,7 +9,10 @@ class Node():
     """
     def __init__(self, data: int):
         self._data = data
-        self._node = None
+        self._next = None
+
+    def __str__(self):
+        return f"{self._data}"
 
 
 class LinkedList():
@@ -18,3 +21,28 @@ class LinkedList():
     """
     def __init__(self):
         self._head = None
+
+    def __str__(self):
+        node = self.head
+        output = []
+        while node is not None:
+            output.append(str(node))
+            node = node._next
+        return " -> ".join(output)
+
+    def push(self, data):
+        if self._head is None:
+            self._head = Node(data)
+        else:
+            node = self._head
+            while node._next is not None:
+                node = node._next
+            node._next = Node(data)
+
+
+if __name__ == '__main__':
+    linked_list = LinkedList()
+    linked_list.push(1)
+    linked_list.push(2)
+    linked_list.push(3)
+    print(linked_list)
