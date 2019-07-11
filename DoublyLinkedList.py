221622debebe6cb271e1f2fa989070f828fa4c87
ASzwarc <szwarc.adam@gmail.com>
2019-07-12 00:26:41 +0200

    Initial implementation of DoublyLinkedList, currently only Node class definition

---
+++ DoublyLinkedList.py
@@ -0,0 +1,34 @@
+"""
+Implementation of doubly linked list
+"""
+
+
+class Node():
+    def __init__(self, data):
+        self._data = data
+        self._prev = None
+        self._next = None
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, value):
+        self._data = value
+
+    @property
+    def next(self):
+        return self._next
+
+    @next.setter
+    def next(self, node):
+        self._next = node
+
+    @property
+    def prev(self):
+        return self._prev
+
+    @prev.setter
+    def prev(self, node):
+        self._prev = node
