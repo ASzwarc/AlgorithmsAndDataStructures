3ab560947f7ea2b266cca45890d7279079c07043
ASzwarc <szwarc.adam@gmail.com>
2019-07-13 15:13:28 +0200

    Added special function __str__ to Node class. Definiton of DoublyLinkedList with implemenation of methods __init__, __str__ and empty. Test code in main.

--- DoublyLinkedList.py
+++ DoublyLinkedList.py
@@ -9,6 +9,9 @@ class Node():
         self._prev = None
         self._next = None
 
+    def __str__(self):
+        return f"{self._data}"
+
     @property
     def data(self):
         return self._data
@@ -32,3 +35,23 @@ class Node():
     @prev.setter
     def prev(self, node):
         self._prev = node
+
+
+class DoublyLinkedList():
+    def __init__(self):
+        self._head = None
+
+    def __str__(self):
+        current_node = self._head
+        output = []
+        while current_node is not None:
+            output.append(str(current_node))
+            current_node = current_node._next
+        return " -> ".join(output)
+
+    def empty(self):
+        return self._head is None
+
+if __name__ == '__main__':
+    double_list = DoublyLinkedList()
+    print(double_list)
