366f453b95ee8bae926b94659766cb23de97b9cb
ASzwarc <szwarc.adam@gmail.com>
2019-07-15 18:53:01 +0200

    Properties and printing function for Node.

--- BinarySearchTree.py
+++ BinarySearchTree.py
@@ -9,6 +9,33 @@ class Node():
         self._left = None
         self._right = None
 
+    def __str__(self):
+        return f"{self._data}"
+
+    @property
+    def data(self):
+        return self._data
+
+    @data.setter
+    def data(self, data):
+        self._data = data
+
+    @property
+    def left(self):
+        return self._left
+
+    @left.setter
+    def left(self, left_node):
+        self._left = left_node
+
+    @property
+    def right(self):
+        return self._right
+
+    @right.setter
+    def right(self, right_node):
+        self._right = right_node
+
 
 class BinarySearchTree():
     def __init__(self):
