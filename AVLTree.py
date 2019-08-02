600d139a9111cc0f3df65077d947360931f56c3d
ASzwarc <szwarc.adam@gmail.com>
2019-08-02 21:58:10 +0200

    Added rebalancing of the tree after inserting element. Refactored Node's unit tests to compare list of tuples (key, height) not strings. AVL: insert operation rebalances tree.

--- AVLTree.py
+++ AVLTree.py
@@ -65,18 +65,20 @@ class Node:
             right_height = self._right._height
         return left_height - right_height
 
-    def evaluate_balance_up(self):
-        node = self._parent
-        while node:
-            balance = node.get_balance()
-            if balance < -1:
-                # left rotation?
-                pass
-            elif balance > 1:
-                # right rotation?
-                pass
+    def align_subtree(self):
+        node = self
+        balance = node.get_balance()
+        if balance < -1:  # right heavy
+            if node._right and node._right.get_balance() > 1:
+                node = node.right_left_rotation()
             else:
-                pass
+                node = node.left_rotation()
+        elif balance > 1:  # left heavy
+            if node._left and node._left.get_balance() < -1:
+                node = node.left_right_rotation()
+            else:
+                node = node.right_rotation()
+        return node
 
     def insert(self, key):
         if self._key == key:
@@ -124,12 +126,15 @@ class AVL:
     def __init__(self):
         self._root = None
 
-    def insert(self, key):
+    def insert(self, key) -> bool:
         if not self._root:
             self._root = Node(key)
             return True
         else:
-            return self._root.insert(key)
+            ret_val = self._root.insert(key)
+            if ret_val:
+                self._root = self._root.align_subtree()
+            return ret_val
 
     def insert_list(self, iterable):
         for item in iterable:
@@ -155,64 +160,72 @@ class TestNode(unittest.TestCase):
         root = Node(1)
         root.insert(2)
         root.insert(3)
-        self.assertEqual(['1 [2]', '2 [1]', '3 [0]'], root.print_inorder())
+        self.assertEqual([(1, 2), (2, 1), (3, 0)],
+                         root.get_key_height_inorder())
         # WHEN
         root = root.left_rotation()
         # THEN
-        self.assertEqual(['1 [0]', '2 [1]', '3 [0]'], root.print_inorder())
+        self.assertEqual([(1, 0), (2, 1), (3, 0)],
+                         root.get_key_height_inorder())
         # case where new root has left child
         # GIVEN
         root = Node(0)
         root.insert(2)
         root.insert(3)
         root.insert(1)
-        self.assertEqual(['0 [2]', '1 [0]', '2 [1]', '3 [0]'],
-                         root.print_inorder())
+        self.assertEqual([(0, 2), (1, 0), (2, 1), (3, 0)],
+                         root.get_key_height_inorder())
         # WHEN
         root = root.left_rotation()
         # THEN
-        self.assertEqual(['0 [1]', '1 [0]', '2 [2]', '3 [0]'],
-                         root.print_inorder())
+        self.assertEqual([(0, 1), (1, 0), (2, 2), (3, 0)],
+                         root.get_key_height_inorder())
 
     def test_right_rotation(self):
         root = Node(5)
         root.insert(4)
         root.insert(3)
-        self.assertEqual(['3 [0]', '4 [1]', '5 [2]'], root.print_inorder())
+        self.assertEqual([(3, 0), (4, 1), (5, 2)],
+                         root.get_key_height_inorder())
         root = root.right_rotation()
-        self.assertEqual(['3 [0]', '4 [1]', '5 [0]'], root.print_inorder())
+        self.assertEqual([(3, 0), (4, 1), (5, 0)],
+                         root.get_key_height_inorder())
         # case where new root has right child
         root = Node(5)
         root.insert(4)
         root.insert(3)
         root.insert(4.5)
-        self.assertEqual(['3 [0]', '4 [1]', '4.5 [0]', '5 [2]'],
-                         root.print_inorder())
+        self.assertEqual([(3, 0), (4, 1), (4.5, 0), (5, 2)],
+                         root.get_key_height_inorder())
         root = root.right_rotation()
-        self.assertEqual(['3 [0]', '4 [2]', '4.5 [0]', '5 [1]'],
-                         root.print_inorder())
+        self.assertEqual([(3, 0), (4, 2), (4.5, 0), (5, 1)],
+                         root.get_key_height_inorder())
 
     def test_right_left_rotation(self):
         # GIVEN
         root = Node(-2)
         root.insert(1)
         root.insert(0)
-        self.assertEqual(['-2 [2]', '0 [0]', '1 [1]'], root.print_inorder())
+        self.assertEqual([(-2, 2), (0, 0), (1, 1)],
+                         root.get_key_height_inorder())
         # WHEN
         root = root.right_left_rotation()
         # THEN
-        self.assertEqual(['-2 [0]', '0 [1]', '1 [0]'], root.print_inorder())
+        self.assertEqual([(-2, 0), (0, 1), (1, 0)],
+                         root.get_key_height_inorder())
 
     def test_left_right_rotation(self):
         # GIVEN
         root = Node(5)
         root.insert(3)
         root.insert(4)
-        self.assertEqual(['3 [1]', '4 [0]', '5 [2]'], root.print_inorder())
+        self.assertEqual([(3, 1), (4, 0), (5, 2)],
+                         root.get_key_height_inorder())
         # WHEN
         root = root.left_right_rotation()
         # THEN
-        self.assertEqual(['3 [0]', '4 [1]', '5 [0]'], root.print_inorder())
+        self.assertEqual([(3, 0), (4, 1), (5, 0)],
+                         root.get_key_height_inorder())
 
 
 class TestAVL(unittest.TestCase):
@@ -227,7 +240,7 @@ class TestAVL(unittest.TestCase):
         avl = AVL()
         avl.insert_list([1, 2, 3, 4])
         self.assertEqual(avl.get_key_height_inorder(),
-                         [(1, 3), (2, 2), (3, 1), (4, 0)])
+                         [(1, 0), (2, 2), (3, 1), (4, 0)])
         avl.insert_list([1, 2, 3, 4])
         self.assertEqual(avl.get_key_height_inorder(),
                          [(1, 3), (2, 2), (3, 1), (4, 0)])
@@ -235,7 +248,7 @@ class TestAVL(unittest.TestCase):
     def test_print(self):
         avl = AVL()
         avl.insert_list([1, 2, 3, 4])
-        self.assertEqual(avl.print_inorder(), "1 [3], 2 [2], 3 [1], 4 [0]")
+        self.assertEqual(avl.print_inorder(), "1 [0], 2 [2], 3 [1], 4 [0]")
 
 if __name__ == '__main__':
     unittest.main()
