55a41d53b7ab42ba2962fa5b181521133ebcdd17
ASzwarc <szwarc.adam@gmail.com>
2019-08-12 22:02:19 +0200

    Node, RedBlackTree: implementation of inserting new node without balancing or recoloring.

--- DataStructures/RedBlackTree.py
+++ DataStructures/RedBlackTree.py
@@ -21,6 +21,24 @@ class Node():
             color = "BLACK"
         return f"{self._key} {color}"
 
+    def insert(self, value):
+        if self._key == value:
+            return False
+        elif self._key < value:
+            if self._right:
+                return self._right.insert(value)
+            else:
+                self._right = Node(value)
+                self._right._parent = self
+                return True
+        else:
+            if self._left:
+                return self._left.insert(value)
+            else:
+                self._left = Node(value)
+                self._left._parent = self
+                return True
+
 
 class RedBlackTree():
     def __init__(self):
@@ -28,6 +46,13 @@ class RedBlackTree():
 
     def empty(self):
         return self._root is None
+
+    def insert(self, value):
+        if self.empty():
+            self._root = Node(value)
+            return True
+        else:
+            return self._root.insert(value)
 
 if __name__ == '__main__':
     node = Node(1)
--- Tests/RedBlackTreeTest.py
+++ Tests/RedBlackTreeTest.py
@@ -9,6 +9,11 @@ class TestNode(unittest.TestCase):
         node._color = NodeColor.RED
         self.assertEqual(str(node), "1 RED")
 
+    def test_insert(self):
+        node = Node(1)
+        self.assertFalse(node.insert(1))
+        self.assertTrue(node.insert(2))
+
 
 class TestRedBlackTree(unittest.TestCase):
     def test_empty(self):
