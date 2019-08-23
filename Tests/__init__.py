eab0ad5290a4e311d6b1c25bd1d2b919289eb84f
ASzwarc <szwarc.adam@gmail.com>
2019-08-23 21:21:53 +0200

    Tests: added import of new unit test.

--- Tests/__init__.py
+++ Tests/__init__.py
@@ -1,2 +1,3 @@
 from .AvlTreeTest import TestNode as TestAvlNode, TestAVL
-from .RedBlackTreeTest import TestNode as TestRedBlackNode, TestRedBlackTree
+from .RedBlackTreeTest import TestNode as TestRedBlackNode
+from .RedBlackTreeTest import TestRedBlackTree, TestRotationAndRecoloring
